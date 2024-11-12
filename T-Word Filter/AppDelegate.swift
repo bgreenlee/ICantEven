4b035acc88d864609177f5dc0c5e0e65ec33aac2
Brad Greenlee <brad@footle.org>
2024-11-12 15:50:04 -0800

    Initial Commit

---
+++ T-Word Filter/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  T-Word Filter
+//
+//  Created by Brad Greenlee on 11/12/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ T-Word FilterTests/T_Word_FilterTests.swift
@@ -0,0 +1,17 @@
+//
+//  T_Word_FilterTests.swift
+//  T-Word FilterTests
+//
+//  Created by Brad Greenlee on 11/12/24.
+//
+
+import Testing
+@testable import T_Word_Filter
+
+struct T_Word_FilterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
